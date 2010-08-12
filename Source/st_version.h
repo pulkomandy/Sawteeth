94725c19714506ca70796c8daa926a435832fc96
pulkomandy <pulkomandy@32c4e4a5-1166-415a-918b-effddbf353a8>
2010-08-12 20:07:37 +0000

    Use the existing st_version.h file since it has other useful information.
    git-svn-id: svn://localhost/sawteeth@4 32c4e4a5-1166-415a-918b-effddbf353a8

--- Source/st_version.h
+++
@@ -1,6 +0,0 @@
-/* Copyright 2010, Adrien Destugues <pulkomandy@pulkomandy.ath.cx>
- * Distributed under the terms of the MIT Licence
- */
-
-// TODO : this should be defined by the build system instead
-#define ST_CURRENT_EDITOR_VERSION 1.3
