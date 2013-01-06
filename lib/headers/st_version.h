11a2ecda510af24a29460cf1f0f064c46aed1d4f
pulkomandy <pulkomandy@32c4e4a5-1166-415a-918b-effddbf353a8>
2013-01-06 14:18:57 +0000

    * Update version to 1.3.  * build libST without setting the TARGET_EDITOR define. Gets us down to 28K, still not enough...
    git-svn-id: svn://localhost/sawteeth@30 32c4e4a5-1166-415a-918b-effddbf353a8

--- lib/headers/st_version.h
+++ lib/headers/st_version.h
@@ -2,13 +2,9 @@
 Distributed under the terms of the MIT Licence. */
 
 // editor version
-#define ST_CURRENT_EDITOR_VERSION 1200
+#define ST_CURRENT_EDITOR_VERSION 1300
 
 // and filetypeversions is not always the same ...
 // changed every time there is a change in the
 // file format
 #define ST_CURRENT_FILE_VERSION 1200
-
-// is defined if source is supposed to save and edit sawteeth files
-#define TARGET_EDITOR
-//#undef TARGET_EDITOR
