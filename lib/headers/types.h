ae18d1be5b26b2b8b8a17b28ed4292bcac186655
Adrien Destugues <pulkomandy@pulkomandy.tk>
2014-08-23 11:08:35 +0200

    Use C99 types to be 64bit safe.
    Thanks to Arvind Norberg for uncovering the issue!

--- lib/headers/types.h
+++ lib/headers/types.h
@@ -4,16 +4,22 @@ Distributed under the terms of the MIT Licence. */
 #ifndef TYPES__JNG
 #define TYPES__JNG
 
-typedef	signed char		int8;
-typedef unsigned char	uint8;
+#include <stdint.h>
 
-typedef	short			int16;
-typedef unsigned short	uint16;
+#ifdef __HAIKU__
+#include <SupportDefs.h>
+#else
+typedef	int8_t		int8;
+typedef uint8_t		uint8;
 
-typedef	long			int32;
-typedef unsigned long	uint32;
+typedef	int16_t		int16;
+typedef uint16_t	uint16;
 
-typedef long			status_t;
+typedef	int32_t		int32;
+typedef uint32_t	uint32;
+
+typedef int32_t		status_t;
+#endif
 
 const	int JNG_OK = 0;
 const	int JNG_ERROR = -1;
